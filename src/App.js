77992ecc535379d856411c0ff393b206af316d66
Miguel Duque <mduquem1@gmail.com>
2020-01-27 09:27:31 -0500

    Delete Blog

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
 import React, { useState } from 'react';
-import { Switch, Route } from 'react-router-dom';
 import { RevealGlobalStyles } from 'react-genie';
 import './App.css';
 import Main from './components/Main/Main';
-import Blog from './components/Blog/Blog';
 import Footer from './components/Footer/Footer';
 import Toolbar from './components/Navigation/Toolbar/Toolbar';
 import Backdrop from './components/UI/Backdrop/Backdrop';
 import SideDrawer from './components/Navigation/SideDrawer/SideDrawer';
 
 function App() {
-  const [showDrawer, setShowDrawer] = useState(false);
+   const [showDrawer, setShowDrawer] = useState(false);
 
-  const sideDrawerToggle = () => {
-    setShowDrawer(showDrawer => {
-      return !showDrawer;
-    });
-  };
+   const sideDrawerToggle = () => {
+      setShowDrawer(showDrawer => {
+         return !showDrawer;
+      });
+   };
 
-  return (
-    <div className="App">
-      <RevealGlobalStyles />
-      {showDrawer ? (
-        <div>
-          <Backdrop show clicked={sideDrawerToggle} />
-          <SideDrawer clicked={sideDrawerToggle} closed={!showDrawer} />
-        </div>
-      ) : null}
-      <Toolbar showDrawer={showDrawer} setShowDrawer={setShowDrawer} />
-      <main>
-        <Switch>
-          <Route exact path="/blog" component={Blog} />
-          <Route exact path="/" component={Main} />
-        </Switch>
-      </main>
-      <footer>
-        <Footer />
-      </footer>
-    </div>
-  );
+   return (
+      <div className='App'>
+         <RevealGlobalStyles />
+         {showDrawer ? (
+            <div>
+               <Backdrop show clicked={sideDrawerToggle} />
+               <SideDrawer clicked={sideDrawerToggle} closed={!showDrawer} />
+            </div>
+         ) : null}
+         <Toolbar showDrawer={showDrawer} setShowDrawer={setShowDrawer} />
+         <main>
+            <Main />
+         </main>
+         <footer>
+            <Footer />
+         </footer>
+      </div>
+   );
 }
 
 export default App;
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,18 +4,15 @@ import NavigationItem from './NavigationItem/NavigationItem';
 import classes from './NavigationItems.module.css';
 
 const NavigationItems = props => {
-  return (
-    <div className={classes.NavigationItems}>
-      <ul>
-        <NavigationItem clicked={props.clicked} link="/">
-          Inicio
-        </NavigationItem>
-        <NavigationItem clicked={props.clicked} link="/blog">
-          Blog
-        </NavigationItem>
-      </ul>
-    </div>
-  );
+   return (
+      <div className={classes.NavigationItems}>
+         <ul>
+            <NavigationItem clicked={props.clicked} link='/'>
+               Inicio
+            </NavigationItem>
+         </ul>
+      </div>
+   );
 };
 
 export default NavigationItems;
