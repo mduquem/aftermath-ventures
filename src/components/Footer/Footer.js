541b5dc37cd4d4efa1ef8b8f2472014043483dd8
Miguel <miguel@houstronomy.com>
2020-08-22 18:36:07 -0500

    add new contact info

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,36 +10,18 @@ const Footer = () => {
     <div className={classes.Footer}>
       <div>
         <h3>
-        Todos los derechos reservados © {new Date().getFullYear()} Aftermath Ventures. 
+          Todos los derechos reservados © {new Date().getFullYear()} Aftermath
+          Ventures.
         </h3>
-        <h3>aftermathventures.col@gmail.com</h3>
-        <h3>Whatsapp: +57 311 231-1571</h3>
-        <h3>Medellín, Colombia</h3>
+        <h3>contacto@aftermathventures.com</h3>
+        <h3>Whatsapp: +57 310 827-4440</h3>
+        <h3>Colombia</h3>
       </div>
 
       <ul className={classes.Redes}>
         <li>
-          <a href="https://wa.me/573112311571">
-            <img
-              src={whatsapp}
-              alt="Comunícate con nosotros por Whatsapp para desarrollar tu emprendimiento o tu página web"
-            />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/aftermathventures/">
-            <img
-              src={instagram}
-              alt="Síguenos en Instagram para acceder al mejor contenido de emprendimiento y tecnología en Colombia y Latinoamerica"
-            />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.facebook.com/aftermathschool/">
-            <img
-              src={facebook}
-              alt="Síguenos en Facebook para acceder al mejor contenido de emprendimiento y tecnología en Colombia y Latinoamerica"
-            />
+          <a href="https://www.linkedin.com/company/aftermathventures">
+            <img src={instagram} alt="" />
           </a>
         </li>
       </ul>
