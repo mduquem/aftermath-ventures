99d11f0ad09f88dc66de85c2c3a568e0a758b2f3
Miguel <miguel@houstronomy.com>
2020-08-28 06:06:04 -0500

    add new form and is not working yet

--- src/components/Contacto/Contacto.js
+++ src/components/Contacto/Contacto.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AnimatedTitle, Reveal, Animation } from 'react-genie';
+import linkedin from '../../assets/svg/linkedin.svg';
 
 import classes from './Contacto.module.css';
 
@@ -11,12 +12,24 @@ const Contacto = () => {
           ¿Qué esperas para entrar en la era digital?
         </AnimatedTitle>
       </h1>
-      <a href="mailto:aftermathventures.col@gmail.com">
+      <div className="mb-5">
+        <a href="https://www.linkedin.com/company/aftermathventures">
+          <img className="mb-3" src={linkedin} alt="" />
+        </a>
+        <h3>desarrollo@aftermathventures.com</h3>
+        <h3>Whatsapp: +57 310 827-4440</h3>
+        <h3>
+          Todos los derechos reservados © {new Date().getFullYear()} Aftermath
+          Ventures.
+        </h3>
+      </div>
+
+      {/* <a href="mailto:desarrollo@aftermathventures.com">
         <Reveal animation={Animation.BounceInRight}>
           {' '}
           <button>ESCRÍBENOS</button>
         </Reveal>
-      </a>
+      </a> */}
     </div>
   );
 };
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,8 +2,21 @@ import React from 'react';
 import { Reveal, Animation } from 'react-genie';
 
 import classes from './Landing.module.css';
+import emailSvg from '../../assets/svg/email.svg';
 
 const Landing = () => {
+  const successfulResult = (
+    <div>
+      <h3>Te escribiremos pronto</h3>
+    </div>
+  );
+  const failedResult = (
+    <div>
+      <h3>
+        Ocurrió un error. Intenta comunicarte a desarrollo@aftermathventures.com
+      </h3>
+    </div>
+  );
   return (
     <div className={classes.Landing}>
       <div className={classes.Lead}>
@@ -18,17 +31,31 @@ const Landing = () => {
           </Reveal>
         </h2>
 
-        <a href="mailto:aftermathventures.col@gmail.com">
-          <Reveal animation={Animation.BounceInLeft}>
-            <button>ESCRÍBENOS</button>
-          </Reveal>
-        </a>
+        <div className={classes.FormGroup}>
+          <div className="input-group ">
+            <div className="input-group-prepend">
+              <img
+                className={['input-group-text', classes.InputGroupText].join(
+                  ' '
+                )}
+                src={emailSvg}
+                alt="email icon"
+              />
+            </div>
+            <input
+              type="email"
+              placeholder="Ingresa tu correo "
+              className={['form-control', classes.FormControl].join(' ')}
+            />
+          </div>
+          <a href="mailto:aftermathventures.col@gmail.com">
+            <Reveal animation={Animation.BounceInLeft}>
+              <button>Nosotros te escribimos</button>
+            </Reveal>
+          </a>
+        </div>
       </div>
       {/* AQUI TOCA PONER OTRO LLAMADO A LA ACCION DISTINTO Toca incluir opt in form y contact form */}
-      {/* <div>
-        <input placeholder="Ingresa tu correo" />
-        <button>Enviar</button>
-      </div> */}
     </div>
   );
 };
