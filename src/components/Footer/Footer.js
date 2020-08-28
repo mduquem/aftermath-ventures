6b31443ba60a987189bbe0181b475d40849b6098
Miguel <miguel@houstronomy.com>
2020-08-28 04:50:08 -0500

    add new order for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,13 +9,13 @@ const Footer = () => {
   return (
     <div className={classes.Footer}>
       <div>
+        <h3>desarrollo@aftermathventures.com</h3>
+        <h3>Whatsapp: +57 310 827-4440</h3>
+        <h3>Colombia</h3>
         <h3>
           Todos los derechos reservados © {new Date().getFullYear()} Aftermath
           Ventures.
         </h3>
-        <h3>desarrollo@aftermathventures.com</h3>
-        <h3>Whatsapp: +57 310 827-4440</h3>
-        <h3>Colombia</h3>
       </div>
 
       <ul className={classes.Redes}>
