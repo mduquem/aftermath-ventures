ebb426d3d4288d1da67c95152064c6e2443a4cd5
Miguel <miguel@houstronomy.com>
2020-09-17 23:53:36 -0500

    add airtable to email field

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,63 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Reveal, Animation } from 'react-genie';
+import axios from 'axios';
 
 import classes from './Landing.module.css';
 import emailSvg from '../../assets/svg/email.svg';
 
 const Landing = () => {
-  const successfulResult = (
-    <div>
-      <h3>Te escribiremos pronto</h3>
-    </div>
-  );
-  const failedResult = (
-    <div>
-      <h3>
-        Ocurrió un error. Intenta comunicarte a desarrollo@aftermathventures.com
-      </h3>
-    </div>
-  );
-  return (
-    <div className={classes.Landing}>
-      <div className={classes.Lead}>
-        <h1>
-          Desarrolla tu futuro hoy<span>.</span>
-        </h1>
-        <h2>
-          <Reveal>
-            No importa si <em>sólo</em> tienes una idea, un prototipo o un
-            negocio establecido, <strong>Aftermath Ventures</strong> te ayuda en
-            cada etapa de tu emprendimiento.
-          </Reveal>
-        </h2>
+	const TODAY = String(new Date());
+	const [email, setEmail] = useState('');
+	const [submitResult, setSubmitResult] = useState('not sent');
 
-        <div className={classes.FormGroup}>
-          <div className="input-group ">
-            <div className="input-group-prepend">
-              <img
-                className={['input-group-text', classes.InputGroupText].join(
-                  ' '
-                )}
-                src={emailSvg}
-                alt="email icon"
-              />
-            </div>
-            <input
-              type="email"
-              placeholder="Ingresa tu correo "
-              className={['form-control', classes.FormControl].join(' ')}
-            />
-          </div>
-          <a href="mailto:aftermathventures.col@gmail.com">
-            <Reveal animation={Animation.BounceInLeft}>
-              <button>Nosotros te escribimos</button>
-            </Reveal>
-          </a>
-        </div>
-      </div>
-      {/* AQUI TOCA PONER OTRO LLAMADO A LA ACCION DISTINTO Toca incluir opt in form y contact form */}
-    </div>
-  );
+	const submitForm = async () => {
+		try {
+			const response = await axios.post(
+				'https://api.airtable.com/v0/appwUsJBXmOq5N6c3/emails?api_key=keyVhyObfMsV33jCY',
+				{ fields: { email, createdAt: TODAY } }
+			);
+
+			setSubmitResult('success');
+
+			console.log('RESPOESNA', response);
+		} catch (error) {
+			console.log('ERRORRR', error);
+			setSubmitResult('error');
+		}
+	};
+	let formContent = (
+		<div className={classes.FormGroup}>
+			<div className="input-group ">
+				<div className="input-group-prepend">
+					<img
+						className={['input-group-text', classes.InputGroupText].join(' ')}
+						src={emailSvg}
+						alt="email icon"
+					/>
+				</div>
+				<input
+					type="email"
+					placeholder="Ingresa tu correo "
+					onChange={(event) => setEmail(event.target.value)}
+					className={['form-control', classes.FormControl].join(' ')}
+				/>
+			</div>
+			<Reveal animation={Animation.BounceInLeft}>
+				<button onClick={submitForm}>Nosotros te escribimos</button>
+			</Reveal>
+		</div>
+	);
+
+	if (submitResult === 'success') {
+		formContent = (
+			<div>
+				<h3>Te escribiremos pronto</h3>
+			</div>
+		);
+	}
+
+	if (submitResult === 'error') {
+		formContent = (
+			<div>
+				<h3>
+					Ocurrió un error. Intenta comunicarte a
+					desarrollo@aftermathventures.com
+				</h3>
+			</div>
+		);
+	}
+
+	return (
+		<div className={classes.Landing}>
+			<div className={classes.Lead}>
+				<h1>
+					Desarrolla tu futuro hoy<span>.</span>
+				</h1>
+				<h2>
+					<Reveal>
+						No importa si <em>sólo</em> tienes una idea, un prototipo o un
+						negocio establecido, <strong>Aftermath Ventures</strong> te ayuda en
+						cada etapa de tu emprendimiento.
+					</Reveal>
+				</h2>
+
+				{formContent}
+			</div>
+			{/* AQUI TOCA PONER OTRO LLAMADO A LA ACCION DISTINTO Toca incluir opt in form y contact form */}
+		</div>
+	);
 };
 
 export default Landing;
