f90c8415c6127f98e7b239e5234e3a07305ec819
Miguel <miguel@houstronomy.com>
2020-09-28 18:08:45 -0500

    delete migrato

--- src/components/Portafolio/Portafolio.js
+++ src/components/Portafolio/Portafolio.js
@@ -9,32 +9,32 @@ import migrato from '../../assets/img/migrato.png';
 import loadup from '../../assets/img/loadup.png';
 
 const Portafolio = () => {
-  return (
-    <div className={classes.Portafolio}>
-      <h1>Portafolio</h1>
-      <ul className={classes.List}>
-        <li>
-          <Reveal>
-            <Project
-              img={domicilios}
-              link="https://domiciliosaudiovisuales.com"
-            />
-          </Reveal>
-        </li>
+	return (
+		<div className={classes.Portafolio}>
+			<h1>Portafolio</h1>
+			<ul className={classes.List}>
+				<li>
+					<Reveal>
+						<Project
+							img={domicilios}
+							link='https://domiciliosaudiovisuales.com'
+						/>
+					</Reveal>
+				</li>
 
-        <li>
+				{/* <li>
           <Reveal>
             <Project img={migrato} link="https://migrato.co" />
           </Reveal>
-        </li>
-        <li>
-          <Reveal>
-            <Project img={loadup} link="https://loadup.today" />
-          </Reveal>
-        </li>
-      </ul>
-    </div>
-  );
+        </li> */}
+				<li>
+					<Reveal>
+						<Project img={loadup} link='https://loadup.today' />
+					</Reveal>
+				</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Portafolio;
