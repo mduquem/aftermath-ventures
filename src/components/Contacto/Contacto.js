8a9982d11e6bd63126681927c4e9fc25de0e9872
Miguel <miguel@houstronomy.com>
2020-10-06 16:34:53 -0500

    second try

--- src/components/Contacto/Contacto.js
+++ src/components/Contacto/Contacto.js
@@ -5,33 +5,25 @@ import linkedin from '../../assets/svg/linkedin.svg';
 import classes from './Contacto.module.css';
 
 const Contacto = () => {
-  return (
-    <div className={classes.Contacto}>
-      <h1>
-        <AnimatedTitle>
-          ¿Qué esperas para entrar en la era digital?
-        </AnimatedTitle>
-      </h1>
-      <div className="mb-5">
-        <a href="https://www.linkedin.com/company/aftermathventures">
-          <img className="mb-3" src={linkedin} alt="" />
-        </a>
-        <h3>desarrollo@aftermathventures.com</h3>
-        <h3>Whatsapp: +57 310 827-4440</h3>
-        <h3>
-          Todos los derechos reservados © {new Date().getFullYear()} Aftermath
-          Ventures.
-        </h3>
-      </div>
+	return (
+		<div className={classes.Contacto}>
+			<h1>
+				<AnimatedTitle>
+					¿Qué esperas para entrar en la era digital?
+				</AnimatedTitle>
+			</h1>
+			<div className='mb-5'>
+				<a href='https://www.linkedin.com/company/aftermathventures'>
+					<img className='mb-3' src={linkedin} alt='' />
+				</a>
 
-      {/* <a href="mailto:desarrollo@aftermathventures.com">
-        <Reveal animation={Animation.BounceInRight}>
-          {' '}
-          <button>ESCRÍBENOS</button>
-        </Reveal>
-      </a> */}
-    </div>
-  );
+				<h3>
+					Aftermath Ventures {new Date().getFullYear()} © Todos los derechos
+					reservados.
+				</h3>
+			</div>
+		</div>
+	);
 };
 
 export default Contacto;
